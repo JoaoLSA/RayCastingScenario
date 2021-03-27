3d2d91c50612fd756e0048a69014007fddd4a60f
Joao Albuquerque <joao.albuquerque@teclat.com.br>
2021-03-27 18:25:03 -0300

    Add positionable camera

--- rtweekend.h
+++ rtweekend.h
@@ -1,7 +1,16 @@
 #ifndef RTWEEKEND_H
 #define RTWEEKEND_H
+//==============================================================================================
+// To the extent possible under law, the author(s) have dedicated all copyright and related and
+// neighboring rights to this software to the public domain worldwide. This software is
+// distributed without any warranty.
+//
+// You should have received a copy (see file COPYING.txt) of the CC0 Public Domain Dedication
+// along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+//==============================================================================================
 
 #include <cmath>
+#include <cstdlib>
 #include <limits>
 #include <memory>
 
@@ -23,6 +32,12 @@ inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
 
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
+}
+
 inline double random_double() {
     // Returns a random real in [0,1).
     return rand() / (RAND_MAX + 1.0);
@@ -33,9 +48,15 @@ inline double random_double(double min, double max) {
     return min + (max-min)*random_double();
 }
 
+inline int random_int(int min, int max) {
+    // Returns a random integer in [min,max].
+    return static_cast<int>(random_double(min, max+1));
+}
+
 // Common Headers
 
 #include "ray.h"
 #include "vec3.h"
+
 
 #endif
